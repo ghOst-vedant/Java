519441207892a3cb709d8d06278738096060da8c
ghOst-vedant <gaikwadvedant30@gmail.com>
2025-04-26 20:32:55 +0530

    [Update]:Started Project directory

--- Arrays/src/ArrayLists.java
+++ Arrays/src/ArrayLists.java
@@ -8,17 +8,17 @@ import java.util.List;
 public class ArrayLists {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+
         list.add(10);
         list.add(5);
         list.add(11);
         list.add(1,20);
-//        Integer[] arr = list.toArray(new Integer[0]);
-        int a = list.get(1);
-        int[] arr = new int[0];
-        System.out.println(arr.getClass());
-//        for(int ele:arr){
-//            System.out.println(ele);
-//        }
+
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+
+        System.out.println("Exited ArrayList.");
 
     }
 }
---
+++ Projects/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+            //shopping cart program
+        Scanner in = new Scanner(System.in);
+        String item;
+        double price;
+        int quantity;
+        char currency = '₹';
+        double total;
+
+        System.out.print("What would you like to buy: ");
+        item = in.nextLine();
+
+        System.out.print("What is the price of each "+item+" ? ");
+        price = in.nextDouble();
+
+        System.out.print("How many would you like to buy ? ");
+        quantity = in.nextInt();
+
+        total = price*quantity;
+        System.out.print("Your total bill: "+total+currency);
+        in.close();
+    }
+}
