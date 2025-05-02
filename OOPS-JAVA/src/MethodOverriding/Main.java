861850bf36b5f851aebf460a7fa5670dcf1d3cfb
ghOst-vedant <gaikwadvedant30@gmail.com>
2025-05-02 15:44:13 +0530

    [New Topic]: Method Overriding

---
+++ OOPS-JAVA/src/MethodOverriding/Main.java
@@ -0,0 +1,14 @@
+package MethodOverriding;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Method overriding = when a subclass provides its on implementation of a method for itself which is already given by the
+                        parent class or the super class.
+
+         */
+        Fish fish1 = new Fish();
+
+        fish1.move();
+    }
+}
